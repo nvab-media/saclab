5d611546f549c8dba145205edaf27af770783981
nvab-media <nvabmedia@gmail.com>
2025-06-11 12:57:41 +0530

    first commit

---
+++ pdf.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def serve_pdf():
+    return send_from_directory(directory='sql', path='pdf1.pdf')
+
+@app.route("2")
+def second_pdf():
+    return send_from_directory(directory='sql', path='pdf2.pdf')
+
+@app.route("3")
+def thrid_pdf():
+    return send_from_directory(directory='sql', path='pdf3.pdf')
+
+if __name__ == "__main__":
+    app.run()
